ffb1a589cae2241b6026457007f5052af9af86c2
Crowdbotics <team@crowdbotics.com>
2021-09-18 21:34:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vit_instagram_after_30667/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vit_instagram_after_30667;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vit_instagram_after_30667";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "vit_instagram_after_30667Identifier",
+  url: "https://vit_instagram_after_30667Identifier.botics.co",
+  api: "https://vit_instagram_after_30667Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
